0dbafba3cea67ec2c8162aa514df5718396bf2e4
avinashbillu <avinashbillu522@gmail.com>
2026-08-08 11:53:55 +0530

    Fix GitHub Pages deployment: relative home links + SPA 404 fallback

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   return (
     <>
       <div className="header">
-        <a href="/#" className="navbar-title" data-cursor="disable">
+        <a href="#landingDiv" className="navbar-title" data-cursor="disable">
           AB
         </a>
         <a
