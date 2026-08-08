611eb0569f81bac80a770790fb2e2a126717d88e
avinashbillu <avinashbillu522@gmail.com>
2026-08-08 11:59:37 +0530

    Update About section content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,13 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I’m **Avinash Billu**, a Computer Science Engineering graduate
-          specializing in **Java Full-Stack Development**. I have hands-on
-          experience with **Java, Spring Boot, Hibernate, React.js, REST APIs,
-          MySQL, JavaScript, HTML, and CSS**, gained through my Java Full Stack
+          I’m Avinash Billu, a Computer Science Engineering graduate
+          specializing in Java Full-Stack Development. I have hands-on
+          experience with Java, Spring Boot, Hibernate, React.js, REST APIs,
+          MySQL, JavaScript, HTML, and CSS, gained through my Java Full Stack
           internship at Tap Academy. I’m passionate about **problem-solving,
           writing clean and maintainable code, learning new technologies, and
-          building scalable, reliable web applications**.
+          building scalable, reliable web applications.
         </p>
       </div>
     </div>
